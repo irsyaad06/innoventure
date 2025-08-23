0391b2ea02a0e94f9fa9d2cd749a9700cfbe911e
irsyaad06 <irsyadfatahilah@gmail.com>
2025-08-23 22:54:10 +0700

    update: single page seminar

--- app/Filament/Resources/SeminarResource/Pages/ListSeminars.php
+++ app/Filament/Resources/SeminarResource/Pages/ListSeminars.php
@@ -9,6 +9,16 @@ use Filament\Resources\Pages\ListRecords;
 class ListSeminars extends ListRecords
 {
     protected static string $resource = SeminarResource::class;
+    public function mount(): void
+    {
+        $setting = \App\Models\Seminar::first();
+        if ($setting) {
+            $this->redirect(static::getResource()::getUrl('edit', ['record' => $setting->id]));
+            return;
+        }
+        $this->redirect(static::getResource()::getUrl('create'));
+    }
+
 
     protected function getHeaderActions(): array
     {
